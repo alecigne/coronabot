e52c9c8e4b31c250effdba1ce6e4f2f22356a37f
Anthony Le Cigne <dev@lecigne.net>
2020-03-22 18:37:58 +0100

    Initial commit

---
+++ src/main/java/net/lecigne/coronamailsender/CoronaMailSenderApplication.java
@@ -0,0 +1,13 @@
+package net.lecigne.coronamailsender;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoronaMailSenderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoronaMailSenderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/lecigne/coronamailsender/CoronaMailSenderApplicationTests.java
@@ -0,0 +1,13 @@
+package net.lecigne.coronamailsender;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoronaMailSenderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
