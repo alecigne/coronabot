23b547717fd46175eb5f2f1ecfa388c4bfd14b4b
Anthony Le Cigne <dev@lecigne.net>
2020-03-22 20:21:38 +0100

    Send an email every day at 6 am

--- src/main/java/net/lecigne/coronamailsender/CoronaMailSenderApplication.java
+++ src/main/java/net/lecigne/coronamailsender/CoronaMailSenderApplication.java
@@ -8,28 +8,15 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableFeignClients
-public class CoronaMailSenderApplication implements CommandLineRunner {
-
-	final CoronaClient coronaClient;
-
-	final CoronaMailSender coronaMailSender;
-
-	@Autowired
-	public CoronaMailSenderApplication(CoronaClient coronaClient, CoronaMailSender coronaMailSender) {
-		this.coronaClient = coronaClient;
-		this.coronaMailSender = coronaMailSender;
-	}
+@EnableScheduling
+public class CoronaMailSenderApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CoronaMailSenderApplication.class, args);
 	}
 
-	@Override
-	public void run(String... args) throws Exception {
-		CoronaInfo coronaInfo = coronaClient.getCoronaInfo("france");
-		coronaMailSender.sendEmail("Informations COVID-19", String.format("Nombre de cas : %d", coronaInfo.getCases()));
-	}
 }
---
+++ src/main/java/net/lecigne/coronamailsender/service/CoronaMailJob.java
@@ -0,0 +1,42 @@
+package net.lecigne.coronamailsender.service;
+
+import lombok.extern.slf4j.Slf4j;
+import net.lecigne.coronamailsender.model.CoronaInfo;
+import net.lecigne.coronamailsender.wsc.CoronaClient;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+
+import javax.mail.MessagingException;
+
+@Service
+@Slf4j
+public class CoronaMailJob {
+
+    final CoronaClient coronaClient;
+
+    final CoronaMailSender coronaMailSender;
+
+    @Autowired
+    public CoronaMailJob(CoronaClient coronaClient, CoronaMailSender coronaMailSender) {
+        this.coronaClient = coronaClient;
+        this.coronaMailSender = coronaMailSender;
+    }
+
+    @Scheduled(cron = "${corona.mail.cron}", zone = "Europe/Paris")
+    public void execute() {
+        log.info("Retrieving information about coronavirus");
+        CoronaInfo coronaInfo = coronaClient.getCoronaInfo("france");
+
+        if (coronaInfo.getCases() != 0) {
+            log.info("Sending an email");
+            try {
+                coronaMailSender.sendEmail("Informations COVID-19", String.format("Nombre de cas : %d", coronaInfo.getCases()));
+                log.info("Email sent successfully");
+            } catch (MessagingException e) {
+                log.error("Error sending email");
+            }
+        }
+    }
+
+}
