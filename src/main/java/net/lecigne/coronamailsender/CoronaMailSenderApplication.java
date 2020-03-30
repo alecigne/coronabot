e8f7cdabe30e797dfc61f19c418dc59e8309e54e
Anthony Le Cigne <dev@lecigne.net>
2020-03-30 23:43:16 +0200

    Send an email when starting up

--- src/main/java/net/lecigne/coronamailsender/CoronaMailSenderApplication.java
+++ src/main/java/net/lecigne/coronamailsender/CoronaMailSenderApplication.java
@@ -1,5 +1,6 @@
 package net.lecigne.coronamailsender;
 
+import lombok.extern.slf4j.Slf4j;
 import net.lecigne.coronamailsender.model.CoronaInfo;
 import net.lecigne.coronamailsender.service.CoronaMailSender;
 import net.lecigne.coronamailsender.wsc.CoronaClient;
@@ -10,13 +11,28 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+import javax.mail.MessagingException;
+
 @SpringBootApplication
 @EnableFeignClients
 @EnableScheduling
-public class CoronaMailSenderApplication {
+@Slf4j
+public class CoronaMailSenderApplication implements CommandLineRunner {
+
+	@Autowired
+	CoronaMailSender coronaMailSender;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CoronaMailSenderApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		try {
+			coronaMailSender.sendEmail("Coronabot", String.format("Coronabot is up and running!"));
+			log.info("Startup email sent successfully");
+		} catch (MessagingException e) {
+			log.error("Error sending startup email");
+		}
+	}
 }
